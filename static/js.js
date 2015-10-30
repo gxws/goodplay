829347cb27c6c784db388cb1615f56899256eb1e
jqjjian-pc <jqjjian@126.com>
2015-10-30 14:35:01 +0800

    20151030
    20151030

--- static/js.js
+++ static/js.js
@@ -2,10 +2,10 @@ return {
 	files: {
 		/* begin 基础模块 */
 		'{static}/web-goodplay/static/js/base.mod.js': [
-			'{static}/static/public/js/jquery.min.js',
-			'{static}/static/public/js/jquery.x.min.js',
+			'{static}/public/js/jquery.min.js',
+			// '{static}/static/public/js/jquery.x.min.js',
 			'{static}/web-goodplay/static/js/base.min.js',
-			'{static}/web-goodplay/static/js/baseAjax.min.js'
+			// '{static}/web-goodplay/static/js/baseAjax.min.js'
 		]
 		/* end 基础模块 */
 	}
